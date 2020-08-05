5522fd83f79dacd795559746c5f72a3ccb08a607
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-08-05 12:20:15 +0500

    key down vs key press

---
+++ script.js
@@ -0,0 +1,18 @@
+// Key press vs key down
+// $("#input-box").keydown(function(event){
+//    var  value=$(this).val();
+//    console.log("Key Down"+event.which);
+//    $("#output1").html("key down"+value); 
+// });
+
+
+// $("#input-box").keypress(function(event){
+//     var  value=$(this).val();
+//     $("#output2").html("key press"+value); 
+//     console.log("Key press"+event.which);
+//  });
+ //key press event delete k saTh trigger nhi hotaa 
+ //is lye key down use hota hai
+ //KEY DOWN ALPHA NUMERIC K ELAWA BHI TRIGGER KARTA HAI
+ //JAB K KEY PRESS SIRF ALPHA NUMERIC KARTA HAI
+//  KEY Press vs key down
